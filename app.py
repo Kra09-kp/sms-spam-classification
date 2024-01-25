1140a7845efc84fa98fb8c162e597001729f156c
Kirti Pogra <88347992+Kra09-kp@users.noreply.github.com>
2024-01-25 16:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def data_preprocess(text):
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.title("Email/SMS  Spam Classifier")
+st.title("SMS  Spam Classifier")
 
 sms = st.text_input("Enter the message ")
 
